4d683f297b005f1ae99da1a7605ace74e2051d11
nizi <failaforcontact@gmail.com>
2020-04-30 08:23:55 +0900

    q

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,6 @@ import Rails from '@rails/ujs';
 
 Rails.start();
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
