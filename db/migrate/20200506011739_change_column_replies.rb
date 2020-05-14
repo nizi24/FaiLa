c22571009979b3a933e2ba3f063ba10f8f9d7a02
nizi <failaforcontact@gmail.com>
2020-05-14 17:00:10 +0900

    notice complete

--- db/migrate/20200506011739_change_column_replies.rb
+++ db/migrate/20200506011739_change_column_replies.rb
@@ -1,6 +1,6 @@
 class ChangeColumnReplies < ActiveRecord::Migration[6.0]
   def change
-    remove_column :replies, :micropost_id, :integer
+    # remove_column :replies, :micropost_id, :integer
 
     add_column :replies, :sended_micropost_id, :integer
     add_column :replies, :received_micropost_id, :integer
