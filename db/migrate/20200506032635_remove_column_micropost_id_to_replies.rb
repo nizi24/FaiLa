d8f6b99dcfca0d6a870fb89f8011228766132bd6
nizi <failaforcontact@gmail.com>
2020-05-14 17:15:17 +0900

    notice complete

--- db/migrate/20200506032635_remove_column_micropost_id_to_replies.rb
+++ db/migrate/20200506032635_remove_column_micropost_id_to_replies.rb
@@ -1,5 +1,5 @@
 class RemoveColumnMicropostIdToReplies < ActiveRecord::Migration[6.0]
   def change
-    remove_column :replies, :micropost_id, :integer
+    # remove_column :replies, :micropost_id, :integer
   end
 end
--- db/migrate/20200506033101_remove_column_microposts_id_replies.rb
+++ db/migrate/20200506033101_remove_column_microposts_id_replies.rb
@@ -1,5 +1,5 @@
 class RemoveColumnMicropostsIdReplies < ActiveRecord::Migration[6.0]
   def change
-    remove_column :replies, :microposts_id, :integer
+    # remove_column :replies, :microposts_id, :integer
   end
 end
